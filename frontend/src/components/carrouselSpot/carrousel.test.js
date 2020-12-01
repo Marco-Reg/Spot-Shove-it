5713daf3b420ce885b790d89e9e18bfaa4bc14ce
Marco Regueiro Fernández <marco-reg@MacBook-Pro-de-Marco.local>
2020-12-01 21:11:56 +0100

    joder sí,100% coverage

---
+++ frontend/src/components/carrouselSpot/carrousel.test.js
@@ -0,0 +1,53 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Simulate } from 'react-dom/test-utils';
+
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import configureStore from 'redux-mock-store';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import CarrouselSpot from './CarrouselSpot';
+
+jest.mock('../../redux/actions/userActions');
+
+const buildStore = configureStore([thunk]);
+
+describe('UserDetail', () => {
+  let wrapper;
+
+  const wrapperFactory = (wrapperInitialState) => {
+    const store = buildStore(wrapperInitialState);
+    store.dispatch = jest.fn();
+
+    return ({ children }) => (
+      <Provider store={store}>
+        <BrowserRouter>
+          {children}
+        </BrowserRouter>
+
+      </Provider>
+    );
+  };
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    wrapper = null;
+  });
+  test('should render submit button', () => {
+    const initialState = { spotReducer: { user: 'asdasd' } };
+    wrapper = wrapperFactory(initialState);
+    render(<CarrouselSpot />, { wrapper });
+
+    expect(document.getElementById('carousel-size')).toBeDefined();
+  });
+  test('should render submit button', () => {
+    const initialState = { spotReducer: { user: 'asdasd' } };
+    wrapper = wrapperFactory(initialState);
+    render(<CarrouselSpot />, { wrapper });
+
+    expect(document.getElementById('carousel-compo')).toBeDefined();
+  });
+});
