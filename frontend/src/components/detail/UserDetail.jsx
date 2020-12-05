d7bb2ff20855411e8ca6aeded6a1a81f9605c43a
Marco Regueiro Fernández <marco-reg@MacBook-Pro-de-Marco.local>
2020-12-05 13:30:46 +0100

    added profile user style

--- frontend/src/components/detail/UserDetail.jsx
+++ frontend/src/components/detail/UserDetail.jsx
@@ -17,20 +17,30 @@ function UserDetail({ user, dispatch }) {
   return (
     <>
       <section className="main-wrapper">
+        <p id="user-name">{user?.userName}</p>
         <img src={user?.userImage} alt="user-avatar" id="user-image" />
+        <div id="linebreak" />
+        <div id="user-spects">
+          <p id="user-description">{user?.userDescription}</p>
+          <div className="linebreak-vertical" />
+          <li id="spects">
+            City/Country :
+            {' '}
+            {user?.UserCity}
+            <li>
+              Stance :
+              {' '}
+              {user?.userStance}
+            </li>
+            <li>
+              Most Favorites skate styles :
+              {' '}
+              {user?.userStyle}
+            </li>
 
-        <p id="user-stance">
-          <div id="user_stance">
-
-            Stance:
-            {user?.userStance}
-          </div>
-        </p>
-
-        <p id="profile-description">
-          {user?.userDescription}
-
-        </p>
+          </li>
+          <li />
+        </div>
 
       </section>
     </>
