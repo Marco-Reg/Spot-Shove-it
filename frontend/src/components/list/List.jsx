f97f5a38dd1ad09aed8807ee220e7d685a3682a9
Marco Regueiro Fernández <marco-reg@MacBook-Pro-de-Marco.local>
2020-12-06 14:59:51 +0100

    problems with routes list

--- frontend/src/components/list/List.jsx
+++ frontend/src/components/list/List.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable react/prop-types */
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 
-function List(){
-    return(
-        
+import { Link } from 'react-router-dom';
+import { requestListSpots } from '../../redux/actions/listActions';
 
-    )
+function List({ spots, dispatch }) {
+  useEffect(() => {
+    dispatch(requestListSpots());
+  }, []);
+  return (
+    <>
+      <section className="list_wrapper">
+        {spots
+            && spots.length >= 0
+            && (
+            <>
+              {spots.map((spot) => (
+                <div className="project_wrapper" id="list_wrapper">
+                  <Link to={`/spot/${spot._d}`}>
+                    <h3 className="spot_title">{spot.spotName}</h3>
+                  </Link>
+                  <p className="spot-info">{spot.spotLocation}</p>
+                  <p className="spot-style">{spot.spotStyle}</p>
+                  <img className="main-spot-image" src={spot.spotImage[0]} alt="" />
+                </div>
+              ))}
+            </>
+            )}
+      </section>
+
+    </>
+  );
 }
-export default List;
+
+function mapStateToProps(state) {
+  return {
+    list: state.listReducer.list,
+  };
+}
+export default connect(mapStateToProps)(List);
--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import userReducer from './userReducer';
 import spotReducer from './spotReducer';
+import listReducer from './listReducer';
 
-const rootReducer = combineReducers({ userReducer, spotReducer });
+const rootReducer = combineReducers({ userReducer, spotReducer, listReducer });
 
 export default rootReducer;
