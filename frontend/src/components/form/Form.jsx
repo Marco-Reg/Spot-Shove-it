d96471ff5f7cc21895b1aa71954a4ddc8a04a356
Marco Regueiro Fernández <marco-reg@MacBook-Pro-de-Marco.local>
2020-12-13 19:53:11 +0100

    it prints in list but not good

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -6,25 +6,42 @@ import { createProject } from '../../redux/actions/formAction';
 import './form.css';
 
 // eslint-disable-next-line no-unused-vars
-function Form({ list, dispatch }) {
+function Form({ dispatch }) {
   const [projectName, setProjectName] = useState('');
   const [projectInfo, setProjectInfo] = useState('');
   const [spotLocation, setLocation] = useState('');
+  const [spotLatitud, setLatitud] = useState('');
+  const [spotLongitude, setLongitude] = useState('');
   const [photo, setPhoto] = useState('');
 
   return (
     <>
       <form className="form_wrapper">
-        <input id="project-name" type="text" value={projectName} onChange={(event) => { setProjectName(event.target.value); }} placeholder="Spot Name" />
-        <input id="project-info" type="text" value={projectInfo} onChange={(event) => { setProjectInfo(event.target.value); }} placeholder="Description" />
-        <input id="project-info" type="text" value={spotLocation} onChange={(event) => { setLocation(event.target.value); }} placeholder="Location" />
-        <input id="project-photo" type="text" value={photo} onChange={(event) => { setPhoto(event.target.value); }} placeholder="photo url" />
         <div id="cordinates">
-          <input id="project-info" type="text" value={spotLocation} onChange={(event) => { setLocation(event.target.value); }} placeholder="latitud" />
-          <input id="project-info" type="text" value={spotLocation} onChange={(event) => { setLocation(event.target.value); }} placeholder="Longitud" />
+
+          <input id="project-name" type="text" value={projectName} onChange={(event) => { setProjectName(event.target.value); }} placeholder="Spot Name" />
+        </div>
+        <div id="cordinates">
+
+          <input id="project-info" type="text" value={projectInfo} onChange={(event) => { setProjectInfo(event.target.value); }} placeholder="Description" />
+        </div>
+        <div id="cordinates">
+
+          <input id="project-info" type="text" value={spotLocation} onChange={(event) => { setLocation(event.target.value); }} placeholder="Location" />
+        </div>
+        <div id="cordinates">
+
+          <input id="project-photo" type="text" value={photo} onChange={(event) => { setPhoto(event.target.value); }} placeholder="photo url" />
+        </div>
+        <div id="cordinates">
+          <input id="project-info" type="text" value={spotLatitud} onChange={(event) => { setLatitud(event.target.value); }} placeholder="latitude" />
+          <input id="project-info" type="text" value={spotLongitude} onChange={(event) => { setLongitude(event.target.value); }} placeholder="Longitude" />
 
         </div>
-        <button type="submit" id="submit-form" onClick={() => { dispatch(createProject({ projectName, projectInfo, photo })); }}>SUBMIT</button>
+        <div id="cordinates">
+
+          <button type="submit" id="submit-form" onClick={() => { dispatch(createProject({ projectName, projectInfo, photo })); }}>SUBMIT</button>
+        </div>
       </form>
 
     </>
